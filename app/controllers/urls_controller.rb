b419ddb2cb0099e20c98770b334039d63cfd8423
gugutz <gugutz@gmail.com>
2019-01-27 21:41:14 -0200

    fix: hits now incrementing corretly

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -11,77 +11,65 @@ class UrlsController < ApplicationController
         url.short_url = encode(url.id)
       end
     end
-    
-
-
 
     @urls
   end
-  
 
   def show
     @url = Url.find(params[:id])
   end
-    
 
   def edit
   end
 
-
   def create
     @url = Url.new(url_params)
-    @url.hits = 0
+    if @url.original_url =~ URI::regexp
+      @url.hits = 0
 
-    # @url.id is only accessible after saving in the database
-    @url.save
+      # @url.id is only accessible after saving in the database
+      @url.save
 
+      redirect_back(fallback_location: root_path)
+      @url
+    else
+      msg = "the URL provided is not a valid URI" 
+      Rails.logger.debug(msg)
+      
+      redirect_back(fallback_location: root_path)
 
-    redirect_back(fallback_location: root_path)
-    # redirect_to @url
+      msg
+    end
   end
 
-  
   def update
   end
   
   def destroy
   end
-
   
   def redirect
     if params[:token].empty? 
-      @teste = "esta vazio"
+      @msg = throw "the :token parameter is empty"
     else
-      @teste = "nao esta vazio"
-    # short_url = request.referer # this catch the incoming url, including its params
-    # url_hash = URI(short_url).query # this returns an string with the incoming url params
-    # url_hash = short_url[32..-1]
       url_hash =  params[:token]
-      decode(url_hash)
+
+      id = decode(url_hash)
+      @url = Url.find(id)
+      @url.hits = @url.hits + 1
+      @url.save
+
+      # redirect_to destination_url.original_url
+      redirect_back(fallback_location: @url.original_url)
     end
   end
   
-      
-  def decode(url_hash)
-    id = Base62.decode(url_hash.to_s)
+  
 
-    if id.nil?
-      logger.info "id is null"
-    else
-      logger.info "id is #{id}"
-      destination_url = Url.find(id)
-      # redirect_to destination_url.original_url
-      redirect_back(fallback_location: destination_url.original_url)
-    end
-
-  end
 
   private
   def url_params
     params.require(:url).permit(:id, :original_url, :hits, :short_url)
-  end
-
-  def get_url_hash(short_url)
   end
 
   def encode(id)
@@ -89,6 +77,19 @@ class UrlsController < ApplicationController
     # @url.short_url = ENV['APP_DOMAIN'] + id_hash
     short_url = "https://hashfier.herokuapp.com/" + id_hash.to_s
   end
+  
+  def decode(url_hash)
+    id = Base62.decode(url_hash.to_s)
+
+    if id.nil?
+      logger.info "id is null"
+    else
+      logger.info "id is #{id}"
+      id
+    end
+
+  end
+
   
   
 
